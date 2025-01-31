824dae5b81b338b3f3b18718ccb2184e6c7d8b54
RobAndonie <a01411863@tec.mx>
2025-01-31 17:57:42 +0100

    footer responsive

--- components/footer.js
+++ components/footer.js
@@ -2,17 +2,24 @@ class Footer extends HTMLElement {
   constructor() {
     super();
   }
+
   connectedCallback() {
     this.innerHTML = `
-            <footer class="footer">
-                <nav class="footer-nav">
-                    <a href="#">Políticas de Privacidad</a>
-                    <a href="#">Aviso de Privacidad</a>
-                    <a href="#">Contáctanos</a>
-                    <a href="#">Créditos</a>
-                </nav>
-            </footer>
-        `;
+      <footer class="footer">
+          <div class="footer-container">
+              <nav class="footer-nav">
+                  <a href="#">Políticas de Privacidad</a>
+                  <a href="#">Aviso de Privacidad</a>
+                  <a href="#">Contáctanos</a>
+                  <a href="#">Créditos</a>
+              </nav>
+              <div class="footer-info">
+                  <p>&copy; ${new Date().getFullYear()} Todos los derechos reservados</p>
+              </div>
+          </div>
+      </footer>
+    `;
   }
 }
+
 customElements.define("footer-comp", Footer);

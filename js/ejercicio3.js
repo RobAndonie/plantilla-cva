0ab56351abb1662418c5af02feba82c15a4ea378
Fioregodm22 <fiorella.demedinac@gmail.com>
2025-01-30 20:08:38 -0600

    ejercicio fiorella
    ejercicio con imagen

---
+++ js/ejercicio3.js
@@ -0,0 +1,19 @@
+let arreglo = ["","",""];
+
+function allowdrop(ev){
+    ev.preventDefault();
+}
+
+function drag(ev){
+    ev.dataTransfer.setData("text",ev.target.id);
+
+}
+
+function drop(ev){
+    if(arreglo[parseInt(ev.target.id)]== ""){
+        var data=ev.dataTransfer.getData("text");
+        arreglo[parseInt(ev.target.id)] =data;
+        ev.target.appendChild(document.getElementById(data));
+    }
+}
+
